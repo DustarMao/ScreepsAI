3661d8e4fd0e1971623f6c7e02958dbe2956c1d0
Dustar Mao <mao.zili@live.cn>
2020-03-09 14:40:43 +0800

    fix: init memory

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import {runCreep} from "./creep"
 import {keepCreeps} from "./spawn"
 
+function init() {
+  if (Memory.object == null) {
+    Memory.object = {}
+  }
+}
+
 /**
  * main loop
  */
 export function loop() {
+  init()
   console.log(`current tick is ${Game.time}!`)
 
   _.values(Game.creeps).forEach(runCreep)
